f3d01cb7e00d8aa918b06da9b809ff6b253cbef0
Ctato1 <dznelashvilitato@gmail.com>
2024-03-21 05:10:53 +0400

    add static lists in recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,10 @@ import {Recipe} from "../recipe.model";
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes:Recipe[] = []
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", 'This is TEST', 'https://www.eatingwell.com/thmb/zvHrm_Z8F2qLeJenpJ6lYodtq7M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/57831531-73819d8ce8f5413cac42cf1c907bc37a.jpg'),
+    new Recipe("Test Recipe", 'This is TEST', 'https://www.eatingwell.com/thmb/zvHrm_Z8F2qLeJenpJ6lYodtq7M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/57831531-73819d8ce8f5413cac42cf1c907bc37a.jpg')
+  ]
 
   constructor() {
 
