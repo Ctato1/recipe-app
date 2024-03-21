a8bc4247f7ed46729b3d397c97f3fdb3c1ce6c65
Ctato1 <dznelashvilitato@gmail.com>
2024-03-21 18:27:14 +0400

    add shopping card list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients = [];
 
+  constructor() {
+  }
 }
