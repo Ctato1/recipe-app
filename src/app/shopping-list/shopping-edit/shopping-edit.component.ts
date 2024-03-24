d121e0c86acff59ed794ba453c13f849d88c1453
Ctato1 <dznelashvilitato@gmail.com>
2024-03-24 22:56:54 +0400

    add ingredient list - user can add ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', {static: false}) nameInputRef!: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef!: ElementRef;
+  @Output() ingredientAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+
+  onAddItem(event: any) {
+    event.preventDefault();
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+
+    console.log(this.nameInputRef.nativeElement.value)
+    console.log(this.amountInputRef.nativeElement.value)
+  }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,4 +14,9 @@ export class ShoppingListComponent {
 
   constructor() {
   }
+  onIngredientAdded(ingredient:Ingredient){
+    this.ingredients.push(ingredient)
+  }
+
+
 }
