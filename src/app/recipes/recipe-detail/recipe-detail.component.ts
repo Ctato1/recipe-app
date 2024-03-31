b2aad33f8f95bd53395d78cf8c21bc72e2ca9ffc
Ctato1 <dznelashvilitato@gmail.com>
2024-03-31 23:28:03 +0400

    add navigation to edit page and new page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../../shopping-list/shopping-list.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {RecipeService} from "../recipe.service";
 
 @Component({
@@ -10,16 +10,17 @@ import {RecipeService} from "../recipe.service";
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent implements OnInit{
+export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe;
   id!: number;
 
-  constructor(private shoppingListService: ShoppingListService,private route:ActivatedRoute,private recipeService:RecipeService) {
+  constructor(private shoppingListService: ShoppingListService, private router: Router,
+              private route: ActivatedRoute, private recipeService: RecipeService) {
   }
 
   ngOnInit() {
     this.recipe = this.route.snapshot.params['id']
-    this.route.params.subscribe((params:Params)=>{
+    this.route.params.subscribe((params: Params) => {
       this.id = +params['id']
       this.recipe = this.recipeService.getRecipe(this.id);
     })
@@ -32,5 +33,9 @@ export class RecipeDetailComponent implements OnInit{
     this.shoppingListService.addRecipeIngredients(ingredients)
   }
 
+  onEditRecipe() {
+    this.router.navigate(['edit'],{relativeTo:this.route})
+    // this.router.navigate(['recipes', this.id, 'edit'])
+  }
 
 }
