74e73eef8abd12d1bcc9411595e71c867c2a4262
Ctato1 <dznelashvilitato@gmail.com>
2024-03-31 21:26:35 +0400

    add navigation with router

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +6,4 @@ import {Component, EventEmitter, Output} from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @Output() urlPath = new EventEmitter<string>();
-  changeLocation(location:string){
-    this.urlPath.emit(location)
-  }
-
 }
