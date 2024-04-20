64103237857b448668c8201e02c4e5c4f94c324f
Ctato1 <dznelashvilitato@gmail.com>
2024-04-20 19:13:05 +0400

    add: recipes in realtime database with header

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,19 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "../recipes/recipe.service";
+import {Recipe} from "../recipes/recipe.model";
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes() {
+    const recipes: Recipe[] = this.recipeService.getRecipes();
+    this.http.put('https://ng-course-recipe-book-35b68-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe((responseRecipes) => {
+      console.log(responseRecipes)
+    })
   }
 }
