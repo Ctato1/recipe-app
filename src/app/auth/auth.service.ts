84a10c4c3e551442bcc9ddcdd9bede1b1c307a53
Ctato1 <dznelashvilitato@gmail.com>
2024-04-25 17:12:19 +0400

    add autologin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,7 +19,7 @@ export interface AuthResponseData {
 export class AuthService {
   user = new BehaviorSubject<User | null>(null)
 
-  constructor(private http: HttpClient,private router:Router) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   signup(email: string, password: string) {
@@ -46,8 +46,27 @@ export class AuthService {
     }))
   }
 
-  logout(){
+  autoLogin() {
+
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+      // @ts-ignore
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+    if (loadedUser.token) {
+      this.user.next(loadedUser)
+    }
+  }
+
+  logout() {
     this.user.next(null);
+    localStorage.removeItem('userData');
     this.router.navigate(['/auth'])
   }
 
@@ -55,6 +74,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
     const user = new User(email, localId, idToken, expirationDate)
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
