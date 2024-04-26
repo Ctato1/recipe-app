a129bf99ec5b75f7b4bae36c58782544738f0abd
Ctato1 <dznelashvilitato@gmail.com>
2024-04-27 03:37:06 +0400

    add: error message delete

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
   selector:'app-alert',
@@ -7,7 +7,11 @@ import {Component, Input} from "@angular/core";
 })
 export class AlertComponent {
   @Input() message!:string | null;
+  @Output() close = new EventEmitter<void>();
   constructor() {
+  }
+  onClose(){
+    this.close.emit();
   }
 
 }
