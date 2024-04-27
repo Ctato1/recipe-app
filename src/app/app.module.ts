14d302b7208e2ac57b162a0f96ac53e89f99121d
Ctato1 <dznelashvilitato@gmail.com>
2024-04-27 21:01:57 +0400

    add: recipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,9 @@ import {AppRoutingModule} from './app-routing.module';
 // components
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 // directives
@@ -26,21 +21,18 @@ import {AuthComponent} from "./auth/auth.component";
 import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {AlertComponent} from "./shared/alert/alert.component";
+import {RecipesModule} from "./recipes/recipes.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
+
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -51,6 +43,7 @@ import {AlertComponent} from "./shared/alert/alert.component";
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    RecipesModule // Import only RecipesModule here
   ],
   providers: [ShoppingListService, RecipeService,
     {
