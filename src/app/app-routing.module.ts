ee67db0ec87dabaa64893729d4c69492ea633a26
Ctato1 <dznelashvilitato@gmail.com>
2024-04-28 01:03:56 +0400

    add: pre load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-
-import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-
-import {AuthComponent} from "./auth/auth.component";
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 
 const routes: Routes = [
   // redirect to recipes if path is empty (pathMatch: 'full')
   {path: '', redirectTo: 'recipes', pathMatch: 'full'},
-  {path: 'recipes', loadChildren: ()=> import('./recipes/recipes.module').then(m => m.RecipesModule)},
-  {path: 'auth', loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule)},
-  {path: 'shopping-list', loadChildren: ()=> import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)},
+  // ლაიზი ლოადის დამხმარე
+  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+  },
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // ააჩქაროს ჩატვირთვა ლაზი ლოადის დამხმარე
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
