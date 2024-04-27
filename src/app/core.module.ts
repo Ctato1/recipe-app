92017f3264abe6bec7d82320f61bb38b3ad59aa2
Ctato1 <dznelashvilitato@gmail.com>
2024-04-28 01:56:31 +0400

    idk

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,8 +1,7 @@
 import {NgModule} from "@angular/core";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
-
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {RecipeService} from "./recipes/recipe.service";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import {CommonModule} from "@angular/common";
 import {LoadingSpinnerComponent} from "./loading-spinner/loading-spinner.component";
 import {DropdownDirective} from "./dropdown.directive";
 import {AlertComponent} from "./alert/alert.component";
+import {AuthCheckModule} from "./auth-check.module";
 
 @NgModule({
-  declarations:[
+  declarations: [
     LoadingSpinnerComponent,
     DropdownDirective,
     AlertComponent
   ],
-  imports:[CommonModule],
-  exports:[
+  // AuthCheckModule if we want to add headers or params every request - I don't know
+  imports: [CommonModule],
+  exports: [
     LoadingSpinnerComponent,
     DropdownDirective,
     AlertComponent,
